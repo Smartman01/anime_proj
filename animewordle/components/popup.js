b8503608df8a4d48340c5162add5ba75189b0be1
Carl Antoine <carlantoine14@gmail.com>
2022-12-11 23:55:45 -0500

    page update, added popup ui, help ui

---
+++ animewordle/components/popup.js
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Popup = ({title, close, children}) => {
+    return (
+        <div style={{
+            backgroundColor: '#5a5858', color: 'white', width: '500px',
+            borderRadius: '10px', border: '2px solid white',
+            display: 'flex', flexDirection: 'column', justifyContent: 'start',
+            alignItems: 'center', padding: 10
+        }}>
+            <h1 style={{textTransform: 'uppercase'}}>{title}</h1>
+            {children}
+            <button className={styles.card}
+                style={{backgroundColor: 'transparent', fontWeight: 'bold', border: '2px solid red'}}
+                onClick={() => close()}
+            >
+                CLOSE
+            </button>
+        </div>
+    );
+}
+
+export default Popup;
