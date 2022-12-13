bd172945eb104b0a20f4cab5a186a6cb174a07de
Carl Antoine <carlantoine14@gmail.com>
2022-12-12 23:35:54 -0500

    guesscharacter api

---
+++ components/gameend.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+const Gameend = ({winlose, name, image, animes}) => {
+    return (
+        <div style={{
+            display: 'flex', flexDirection: 'column', alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            {/* <Image /> */}
+        </div>
+    );
+}
+
+export default Gameend;
