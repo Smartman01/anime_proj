09e68a76b755de0216e9564466c260a3cfd27cc4
Carl Antoine <carlantoine14@gmail.com>
2022-12-23 01:00:20 -0500

    updated ui and api bool

--- animewordle/components/gameend.js
+++ animewordle/components/gameend.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { useState } from 'react';
 import styles from '../styles/Home.module.css'
 
 const imageLoader = ({ src }) =>
@@ -6,7 +7,10 @@ const imageLoader = ({ src }) =>
     return src;
 }
 
-const Gameend = ({winlose}) => {
+const Gameend = ({winlose, minWidth}) => {
+
+    const [len, setLen] = useState(3);
+
     return (
         <div style={{
             display: 'flex', flexDirection: 'column', alignItems: 'center',
@@ -14,21 +18,22 @@ const Gameend = ({winlose}) => {
         }}>
             <img src={`data:image/png;base64,${winlose.character.image}`}
                 style={{
-                    border: '2px solid lime', borderRadius: 10,
+                    border: `2px solid ${winlose.failed ? 'red' : 'lime'}`, borderRadius: 10,
+                    width: minWidth !== null ? '125px' : 175
                 }}
             />
             <h2>{winlose.character.name}</h2>
-            <h3 style={{textTransform: 'uppercase', margin: 0}}>Anime they are in</h3>
-            <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', maxWidth: 300}}>
+            {/* <h3 style={{textTransform: 'uppercase', margin: 0}}>Anime they are in</h3> */}
+            <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', maxWidth: 250, maxHeight: 150, overflow: 'auto'}}>
                 {
-                    winlose.character.animes.slice(0, 3).map((item, index) =>
+                    winlose.character.animes.slice(0, len).map((item, index) =>
                     {
                         return (
                             <p key={index}
                                 style={{
-                                    border: '2px solid lime', borderRadius: 10,
-                                    fontSize: 12, padding: 10, margin: 10,
-                                    fontWeight: 'bold'
+                                    border: `2px solid ${winlose.failed ? 'red' : 'lime'}`, borderRadius: 10,
+                                    fontSize: 12, padding: 10, margin: 10, textAlign: 'center',
+                                    fontWeight: 'bold', alignSelf: 'center'
                                 }}
                             >
                                 {item}
@@ -38,9 +43,11 @@ const Gameend = ({winlose}) => {
                 }
                 <button className={styles.card}
                     style={{
-                        backgroundColor: 'transparent', padding: 15,
-                        border: '2px solid lime', fontWeight: 'bold'
+                        backgroundColor: 'transparent', padding: 10,
+                        border: `2px solid ${winlose.failed ? 'red' : 'lime'}`, fontWeight: 'bold',
+                        display: len === winlose.character.animes.length ? 'none' : null
                     }}
+                    onClick={() => setLen(winlose.character.animes.length)}
                 >
                     more...
                 </button>
