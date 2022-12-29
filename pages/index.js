d4e5c80dbfd636a2dba832f81c880f90a81eab40
Carl Antoine <carlantoine14@gmail.com>
2022-12-29 15:43:40 -0500

    rm feedback for now

--- pages/index.js
+++ pages/index.js
@@ -183,7 +183,7 @@ export default function Home() {
                 >
                   Results
                 </button>
-                <button
+                {/* <button
                   className={styles.menuBtn}
                   onClick={() => {
                     closeDropdown();
@@ -191,7 +191,7 @@ export default function Home() {
                   }}
                 >
                   Feedback
-                </button>
+                </button> */}
               </div>
             </>
             :
@@ -209,12 +209,12 @@ export default function Home() {
               >
                 Results
               </button>
-              <button
+              {/* <button
                 className={styles.menuBtn}
                 onClick={() => setPopup({ show: true, title: 'Feedback' })}
               >
                 Feedback
-              </button>
+              </button> */}
             </div>
         }
       </div>
